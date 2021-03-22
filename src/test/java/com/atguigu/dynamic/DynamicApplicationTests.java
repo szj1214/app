c725609b946ee546b12889db19b6d5c3ba5adb4e
司泽健 <1187379852@qq.com>
2021-03-22 14:11:17 +0800

    初始化

---
+++ src/test/java/com/atguigu/dynamic/DynamicApplicationTests.java
@@ -0,0 +1,13 @@
+package com.atguigu.dynamic;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DynamicApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
